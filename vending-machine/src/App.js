5f9cbe3188d491adff33f4e549eb48224edbcabc
MinWook6457 <minuk6457@gmail.com>
2024-03-30 21:18:14 +0900

    [기능수정] : 초기 페이지 => 페이지 로드 시 데이터베이스에 데이터 삽입 => 추후 음료 카운팅을 위해 개선

--- vending-machine/src/App.js
+++ vending-machine/src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Container, Row, Col, Navbar, Button, Card } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
-
-
-
+import axios from 'axios'
 
 import './App.css';
 
 function Drink({ imageUrl, description, price }) {
+  const handleSelect = () => {
+    axios.post('/machine/selectedBeverage', { description, price }) // axios로 변경
+    .then(response => {
+      // 응답 처리
+      if (response.status === 200) { // 응답이 성공했을 때
+        console.log('음료가 선택되었습니다.');
+      } else {
+        console.error('음료 선택에 실패했습니다.');
+      }
+    })
+    .catch(error => {
+      // 에러 처리
+      console.error('요청 중 오류가 발생했습니다.', error);
+    });
+  };
+
+
   return (
     <Col md={4} className="mb-4">
       <Card className="shadow-sm">
@@ -16,7 +31,7 @@ function Drink({ imageUrl, description, price }) {
           <Card.Text>{description}</Card.Text>
           <div className="d-flex justify-content-between align-items-center">
             <div className="btn-group">
-              <Button variant="outline-secondary" size="sm">View</Button>
+              <Button variant="outline-secondary" size="sm"  onClick={handleSelect}>Select</Button>
               <Button variant="outline-secondary" size="sm">Edit</Button>
             </div>
             <small className="text-muted">{price}원</small>
@@ -28,6 +43,18 @@ function Drink({ imageUrl, description, price }) {
 }
 
 function App() {
+  useEffect(() => {
+    // 페이지가 로드될 때 데이터베이스에 데이터 삽입 요청 보내기
+    axios.post('/machine/initialize')
+      .then(response => {
+        console.log(response)
+        console.log('데이터베이스에 데이터가 성공적으로 삽입되었습니다.');
+      })
+      .catch(error => {
+        console.error('데이터베이스 데이터 삽입에 실패했습니다.', error);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header>
@@ -42,20 +69,20 @@ function App() {
       <div className="album py-5 bg-light">
         <Container>
           <Row>
-            <Drink imageUrl="./water.png" description="물이에용!!" price={450} />
-            <Drink imageUrl="./coffee.png" description="아이스 아메리카노 입니다." price={500} />
-            <Drink imageUrl="./sports.png" description="이온 음료 입니다 ㅎㅎ" price={550} />
+            <Drink imageUrl="./water.png" description="water" price={450} />
+            <Drink imageUrl="./coffee.png" description="coffee" price={500} />
+            <Drink imageUrl="./sports.png" description="ionic" price={550} />
           </Row>
           <Row>
-            <Drink imageUrl="./shake.png" description="딸기 와 우유를 섞은 딸기 쉐이크 입니다." price={700} />
-            <Drink imageUrl="./sports.png" description="이온음료입니다." price={750} />
-            <Drink imageUrl="./ade.png" description="특화 에이드" price={800} />
+            <Drink imageUrl="./shake.png" description="shake" price={700} />
+            <Drink imageUrl="./cola.png" description="cola" price={750} />
+            <Drink imageUrl="./ade.png" description="ade" price={800} />
           </Row>
         </Container>
       </div>
       <footer className="text-muted">
         <Container>
-          <p>Album example is © Bootstrap, but please download and customize it for yourself!</p>
+          <p>Vending Machine Using SocketIO made by MinWook CSE 19</p>
         </Container>
       </footer>
     </div>
