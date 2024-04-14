cf2e1b79dc506d6a190f40ed0cb4f128236e4314
MinWook6457 <minuk6457@gmail.com>
2024-04-14 17:20:06 +0900

    [기능추가] : 서버측에 보낼 페이로드 설정 (해쉬 테이블 사용) - 해쉬 키 : uuid

--- vending/src/preload.js
+++ vending/src/preload.js
@@ -1,2 +1,28 @@
 // See the Electron documentation for details on how to use preload scripts:
 // https://www.electronjs.org/docs/latest/tutorial/process-model#preload-scripts
+
+const {contextBridge , ipcRenderer } = require('electron') 
+const { v4: uuidv4 } = require('uuid')
+
+async function connectToMainProcess(content, vendingData){
+    const uuid = generateRandomUUID()
+
+    const payload = {
+        hash : "",
+        uuid,
+        content,
+        payload : vendingData
+    }
+
+    return await ipcRenderer.invoke(content, payload);
+}
+
+contextBridge.exposeInMainWorld('vending', {
+    processId : async (uuid) => {
+        return await connectToMainProcess("processId",uuid)
+    }
+})
+
+function generateRandomUUID() {
+    return uuidv4()
+}
---
+++ vending/worker/worker.js
@@ -0,0 +1,18 @@
+﻿/*
+preload -> workerpool -> worker.js
+
+프로미스에 await를 걸어 끝날때 까지 대기
+
+리액트에서 받아온 데이터 hash -> map에 저장 (해쉬화)
+
+무엇을 언제 어디에
+
+cmd : 행동
+vendingId : 자판기 id
+date : 생성 시각
+
+ipc 통신 시에 변수에 함수가 저장되면 안됨
+*/
+
+const { Worker } = require('worker_threads')
+
